f3d753703bf2052a5eb97eebff24f56266650226
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-12-14 19:43:39 -0500

    update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,5 +4,5 @@
 -- The password of hbnb_dev is: hbnb_dev_pwd
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev_db'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON hbnb_dev_db.* TO 'hbnb_dev_db'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev_db'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev_db'@'localhost';
