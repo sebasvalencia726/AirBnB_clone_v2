740a56d2ed4f3afa88e660cd6a0f236a5e6328fa
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-12-14 20:13:53 -0500

    MySQL setup test task

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- script that prepares a MySQL server for the project:
+-- A database hbnb_test_db
+-- A new user hbnb_test (in localhost)
+-- The password of hbnb_test is: hbnb_test_pwd
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+USE hbnb_test_db;
