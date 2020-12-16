568196c87bac861bb00d7a62db83bc9b72b64179
hfog2066 <hfog2000@yahoo.com>
2020-12-16 18:01:31 -0500

    __iniy__.py modified

--- models/__init__.py
+++ models/__init__.py
@@ -1,27 +1,13 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage
-Depending of the value of the environment variable HBNB_TYPE_STORAGE:
-    If equal to db:
-        Import DBStorage class in this file
-        Create an instance of DBStorage and store it in the
-        variable storage.
-    Else:
-        Import FileStorage class in this file
-        Create an instance of FileStorage and store it in the variable storage
-"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
-from models.place import Place
-from models.user import User
-from models.review import Review
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.state import State
-import os
+"""This module instantiates an object of class FileStorage"""
+from os import getenv
 
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    storage.reload()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-storage.reload()
+    storage.reload()
