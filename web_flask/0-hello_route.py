8770b79121186734a56d718e11f9057af2a52115
sebasvalencia726 <sebasvalencia726@gmail.com>
2021-01-19 14:14:37 -0500

    first task

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,9 +3,11 @@
 listening on 0.0.0.0, port 5000
 """
 from flask import Flask
+
+
 app = Flask(__name__)
 @app.route('/', strict_slashes=False)
-def hello_HBNB():
+def index():
     """script that starts a Flask web application:
 listening on 0.0.0.0, port 5000
     """
