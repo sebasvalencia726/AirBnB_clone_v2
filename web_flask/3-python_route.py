4fe4a0cc315648383e37ead8103e76294c31008c
sebasvalencia726 <sebasvalencia726@gmail.com>
2021-01-19 21:04:01 -0500

    4th task

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application:
+listening on 0.0.0.0, port 5000
+Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+    /c/<text>: display “C ” followed by the value of the text variable
+
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """/: display “Hello HBNB!”
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """/hbnb: display “HBNB”
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_is_fun(text):
+    """display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    text = text.replace("_", " ")
+    return 'C {}'.format(text)
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>/", strict_slashes=False)
+def post_form(text="is_cool"):
+    """display “Python ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    text = text.replace("_", " ")
+    return 'Python {}'.format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
